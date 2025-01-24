632ec4c8644e1d693c6dc022488373db83971c8f
alcoope <alcoope@microsoft.com>
2025-01-24 10:27:58 -0500

    Toolbar refactor for email pane done.

--- app/src/main/java/com/example/alexarchitecture/MainPane.kt
+++ app/src/main/java/com/example/alexarchitecture/MainPane.kt
@@ -1,29 +1,22 @@
 package com.example.alexarchitecture
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DismissibleDrawerSheet
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuite
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
@@ -33,18 +26,15 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.alexarchitecture.contributions.ScreenContribution
 import com.example.alexarchitecture.ui.theme.AlexArchitectureTheme
 import kotlinx.coroutines.launch
 
@@ -136,9 +126,7 @@ fun MainPane() {
                 },
                 drawerState = drawerState
             ) {
-                MainPaneContent(
-                    currentScreen = currentScreen, drawerState = drawerState, navigationSuiteType = navSuiteType
-                )
+                currentScreen.content.invoke()
             }
         } else {
             MyDismissibleNavigationDrawer(drawerContent = {
@@ -150,70 +138,14 @@ fun MainPane() {
                 }
             },
                 drawerState = drawerState) {
-                MainPaneContent(currentScreen = currentScreen, drawerState = drawerState, navigationSuiteType = navSuiteType)
+                currentScreen.content.invoke()
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun MainPaneContent(
-    currentScreen: ScreenContribution,
-    drawerState: DrawerState,
-    navigationSuiteType: NavigationSuiteType,
-    modifier: Modifier = Modifier
-) {
-    Scaffold(
-        modifier = modifier,
-        floatingActionButton = {
-            currentScreen.fabContribution?.let { fabContribution ->
-                FloatingActionButton(onClick = { /*TODO*/ }) {
-                    AnimatedContent(targetState = fabContribution.icon) { targetState ->
-                        Icon(painter = painterResource(id = targetState), contentDescription = fabContribution.description)
-                    }
-                }
-            }
-        },
-        topBar = {
-            TopAppBar(
-                title = { Text(text = currentScreen.toolbarContribution?.title?.collectAsStateWithLifecycle()?.value ?: currentScreen.title) },
-                navigationIcon = {
-                    if (navigationSuiteType == NavigationSuiteType.NavigationBar) {
-                        val scope = rememberCoroutineScope()
-                        HamburgerButton {
-                            scope.launch {
-                                if (drawerState.isClosed) {
-                                    drawerState.open()
-                                } else {
-                                    drawerState.close()
-                                }
-                            }
-                        }
-                    }
-                },
-                actions = {
-                    currentScreen.toolbarContribution?.let { toolbarContribution ->
-                        if (toolbarContribution.actions.isNotEmpty()) {
-                            toolbarContribution.actions.forEach { toolbarAction ->
-                                IconButton(onClick = toolbarAction.onClick) {
-                                    Icon(painter = painterResource(id = toolbarAction.icon), contentDescription = toolbarAction.title)
-                                }
-                            }
-                        }
-                    }
-                }
-            )
-        }
-    ) { padding ->
-        Box(modifier = Modifier.padding(padding)) {
-            currentScreen.content.invoke()
-        }
-    }
-}
-
-@Composable
-private fun HamburgerButton(onClick: () -> Unit) {
+fun HamburgerButton(onClick: () -> Unit) {
     IconButton(onClick = onClick) {
         Icon(painter = painterResource(id = R.drawable.outline_menu_24), contentDescription = "Menu")
     }
--- app/src/main/java/com/example/alexarchitecture/contributions/ScreenContribution.kt
+++ app/src/main/java/com/example/alexarchitecture/contributions/ScreenContribution.kt
@@ -4,11 +4,13 @@ import androidx.annotation.DrawableRes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.alexarchitecture.R
 import com.example.alexarchitecture.email.EmailPane
 import com.example.alexarchitecture.email.EmailViewModel
+import kotlinx.coroutines.launch
 
 sealed class ScreenContribution(
     val route: String,
@@ -16,27 +18,37 @@ sealed class ScreenContribution(
     @DrawableRes
     val icon: Int,
     val content: @Composable () -> Unit,
-    val drawerContribution: DrawerContribution? = null,
-    val fabContribution: FABContribution? = null,
-    val toolbarContribution: ToolbarContribution? = null
+    val drawerContribution: DrawerContribution? = null
 ) {
     object Email : ScreenContribution(
         route = "email", title = "Email", icon = R.drawable.outline_email_24, content = {
             val emailViewModel: EmailViewModel = viewModel()
             val emailUiState by emailViewModel.uiState.collectAsStateWithLifecycle()
-            ToolbarContribution.Email.updateTitle(emailUiState.selectedFolder?.title ?: "Email")
+            val scope = rememberCoroutineScope()
 
             EmailPane(
                 onEmailSelect = { email ->
                     emailViewModel.updateSelectedEmail(email)
-                }, selectedEmail = emailUiState.selectedEmail, folderEmails = emailUiState.folderEmails
+                },
+                selectedEmail = emailUiState.selectedEmail,
+                folderEmails = emailUiState.folderEmails,
+                onDrawerButtonClick = {
+                    scope.launch {
+                        if (DrawerContribution.Email.isDrawerOpen()) {
+                            DrawerContribution.Email.closeDrawer()
+                        } else {
+                            DrawerContribution.Email.openDrawer()
+                        }
+                    }
+                },
+                toolbarTitle = emailUiState.selectedFolder?.title ?: "Email"
             )
-        }, drawerContribution = DrawerContribution.Email, fabContribution = FABContribution.Email, toolbarContribution = ToolbarContribution.Email
+        }, drawerContribution = DrawerContribution.Email
     )
 
     object Calendar : ScreenContribution("calendar", "Calendar", R.drawable.outline_calendar_month_24, {
         Text(text = "Calendar")
-    }, fabContribution = FABContribution.Calendar, toolbarContribution = ToolbarContribution.Calendar)
+    })
 
     object Feed : ScreenContribution("feed", "Feed", R.drawable.outline_dynamic_feed_24, {
         Text(text = "Feed")
